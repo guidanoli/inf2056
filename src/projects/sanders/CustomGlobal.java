a390b45df84ecbeeaf4750eb4e6ef7bd48466832
Guilherme Dantas <guidanoli@hotmail.com>
2022-05-04 19:23:46 -0300

    Add `sanders` project

---
+++ src/projects/sanders/CustomGlobal.java
@@ -0,0 +1,34 @@
+package projects.sanders;
+
+import sinalgo.runtime.AbstractCustomGlobal;
+
+/**
+ * This class holds customized global state and methods for the framework. 
+ * The only mandatory method to overwrite is 
+ * <code>hasTerminated</code>
+ * <br>
+ * Optional methods to override are
+ * <ul>
+ * <li><code>customPaint</code></li>
+ * <li><code>handleEmptyEventQueue</code></li>
+ * <li><code>onExit</code></li>
+ * <li><code>preRun</code></li>
+ * <li><code>preRound</code></li>
+ * <li><code>postRound</code></li>
+ * <li><code>checkProjectRequirements</code></li>
+ * </ul>
+ * @see sinalgo.runtime.AbstractCustomGlobal for more details.
+ * <br>
+ * In addition, this class also provides the possibility to extend the framework with
+ * custom methods that can be called either through the menu or via a button that is
+ * added to the GUI. 
+ */
+public class CustomGlobal extends AbstractCustomGlobal{
+	
+	/* (non-Javadoc)
+	 * @see runtime.AbstractCustomGlobal#hasTerminated()
+	 */
+	public boolean hasTerminated() {
+		return false;
+	}
+}
---
+++ src/projects/sanders/LogL.java
@@ -0,0 +1,10 @@
+package projects.sanders;
+
+/**
+ * Enumerates the log-levels. Levels above THRESHOLD will be included
+ * in the log-file. The levels below (with a higher enumeration value) not.
+ */
+public class LogL extends sinalgo.tools.logging.LogL{
+}
+
+
