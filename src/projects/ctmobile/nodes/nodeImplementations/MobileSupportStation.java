13b074806cdaf12a7460d9799ad7140c2a4e02f2
Guilherme Dantas <guidanoli@hotmail.com>
2022-06-22 09:19:02 -0300

    Adjusting some stuff

--- src/projects/ctmobile/nodes/nodeImplementations/MobileSupportStation.java
+++ src/projects/ctmobile/nodes/nodeImplementations/MobileSupportStation.java
@@ -21,18 +21,26 @@ import sinalgo.tools.logging.Logging;
 
 public class MobileSupportStation extends Node {
 
+	/**
+	 * Static variables
+	 */
+	
 	// all mobile support station nodes
 	private static Vector<MobileSupportStation> allMSSs = new Vector<MobileSupportStation>();
 	
+	/**
+	 * Instance variables
+	 */
+	
 	// Set containing the identities of the mobile hosts located in the cell of MSS_i.
-	HashSet<MobileHost> localMHs;
+	private HashSet<MobileHost> localMHs;
 	
 	// Sequence number which identifies the current round executed by MSS_i.
-	int r;
+	private int r;
 	
 	// Phase number in a round. When the protocol starts or ends, Phase_i is equal to 0.
 	// Otherwise this variable is either equal to 1, 2, 3 or 4.
-	int phase;
+	private int phase;
 
 	enum State {
 		Undecided,
@@ -41,33 +49,33 @@ public class MobileSupportStation extends Node {
 	
 	// This variable is set to decided if the consensus has terminated.
 	// Otherwise it is set to undecided.
-	State state;
+	private State state;
 	
 	// Sequence number of the last round during which a new estimate sent by a coordinator
 	// has been accepted as the new value of V_i.
-	int ts;
+	private int ts;
 	
 	// Set containing the identities of the mobile hosts whose initial values are already known by MSS_i.
 	// MSS_i collects values of the mobile hosts located in its cell until endCollect = true.
-	HashSet<MobileHost> p;
+	private HashSet<MobileHost> p;
 	
 	// Set containing the collected values.
-	HashSet<Integer> newV;
+	private HashSet<Integer> newV;
 	
 	// Last set of values proposed by MSS_i.
-	HashSet<Integer> v;
+	private HashSet<Integer> v;
 	
 	// Set containing the estimates received by the coordinator MSS_i during the round r.
-	HashMap<Integer, HashSet<Estimate>> log;
+	private HashMap<Integer, HashSet<Estimate>> log;
 	
 	// Number of positive acknowledgments received by the coordinator MSS_i during round r.
-	HashMap<Integer, Integer> nPositive;
+	private HashMap<Integer, Integer> nbP;
 
 	// Number of negative acknowledgments received by the coordinator MSS_i during round r.
-	HashMap<Integer, Integer> nNegative;
+	private HashMap<Integer, Integer> nbN;
 	
 	// If MSS_i should stop collecting proposals from mobile hosts
-	boolean endCollect;
+	private boolean endCollect;
 	
 	private Logging logger = Logging.getLogger("ctmobile.log");
 	
@@ -145,10 +153,10 @@ public class MobileSupportStation extends Node {
 		v = new HashSet<Integer>();
 		log = new HashMap<Integer, HashSet<Estimate>>();
 		log.put(r, new HashSet<Estimate>());
-		nPositive = new HashMap<Integer, Integer>();
-		nPositive.put(r, 0);
-		nNegative = new HashMap<Integer, Integer>();
-		nNegative.put(r, 0);
+		nbP = new HashMap<Integer, Integer>();
+		nbP.put(r, 0);
+		nbN = new HashMap<Integer, Integer>();
+		nbN.put(r, 0);
 		endCollect = false;
 	}
 
